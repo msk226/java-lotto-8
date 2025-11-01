bd096edb2602a040bc0f83cf76329e9d47c3ff0b
msk226 <kminseok5167@gmail.com>
2025-11-01 15:52:18 +0900

    refactor(io): 출력되는 메세지 내부 상수화 진행

--- src/main/java/lotto/io/input/InputView.java
+++ src/main/java/lotto/io/input/InputView.java
@@ -3,18 +3,22 @@ package lotto.io.input;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+    private static final String MESSAGE_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String MESSAGE_WINNING_NUMBERS = "\n당첨 번호를 입력해 주세요.";
+    private static final String MESSAGE_BONUS_NUMBER = "\n보너스 번호를 입력해 주세요.";
+
     public String readPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(MESSAGE_PURCHASE_AMOUNT);
         return Console.readLine();
     }
 
     public String readWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(MESSAGE_WINNING_NUMBERS);
         return Console.readLine();
     }
 
     public String readBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println(MESSAGE_BONUS_NUMBER);
         return Console.readLine();
     }
 }
--- src/main/java/lotto/io/output/OutputView.java
+++ src/main/java/lotto/io/output/OutputView.java
@@ -7,24 +7,35 @@ import lotto.domain.LottoResult;
 import lotto.domain.enums.Rank;
 
 public class OutputView {
+    public static final int DEFAULT_VALUE = 0;
+    private static final String MESSAGE_PURCHASED = "%d개를 구매했습니다.";
+    private static final String MESSAGE_STATISTICS_HEADER = "당첨 통계";
+    private static final String MESSAGE_DIVIDER = "---";
+    private static final String MESSAGE_YIELD_FORMAT = "총 수익률은 %.1f%%입니다.%n";
+    private static final String MATCH_3 = "3개 일치 (5,000원) - %d개";
+    private static final String MATCH_4 = "4개 일치 (50,000원) - %d개";
+    private static final String MATCH_5 = "5개 일치 (1,500,000원) - %d개";
+    private static final String MATCH_5_BONUS = "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개";
+    private static final String MATCH_6 = "6개 일치 (2,000,000,000원) - %d개";
+
     public void printPurchased(List<Lotto> tickets) {
-        System.out.println(tickets.size() + "개를 구매했습니다.");
+        System.out.printf((MESSAGE_PURCHASED) + "%n", tickets.size());
         for (Lotto t : tickets) {
             System.out.println(t.numbers());
         }
     }
 
     public void printStatistics(LottoResult result, double yield) {
-        System.out.println("당첨 통계");
-        System.out.println("---");
+        System.out.println(MESSAGE_STATISTICS_HEADER);
+        System.out.println(MESSAGE_DIVIDER);
+
         Map<Rank, Integer> m = result.view();
+        System.out.printf((MATCH_3) + "%n", m.getOrDefault(Rank.FIFTH, DEFAULT_VALUE));
+        System.out.printf((MATCH_4) + "%n", m.getOrDefault(Rank.FOURTH, DEFAULT_VALUE));
+        System.out.printf((MATCH_5) + "%n", m.getOrDefault(Rank.THIRD, DEFAULT_VALUE));
+        System.out.printf((MATCH_5_BONUS) + "%n", m.getOrDefault(Rank.SECOND, DEFAULT_VALUE));
+        System.out.printf((MATCH_6) + "%n", m.getOrDefault(Rank.FIRST, DEFAULT_VALUE));
 
-        System.out.println("3개 일치 (5,000원) - " + m.get(Rank.FIFTH) + "개");
-        System.out.println("4개 일치 (50,000원) - " + m.get(Rank.FOURTH) + "개");
-        System.out.println("5개 일치 (1,500,000원) - " + m.get(Rank.THIRD) + "개");
-        System.out.println("5개 일치, 보너스 볼 일치 (30,000,000원) - " + m.get(Rank.SECOND) + "개");
-        System.out.println("6개 일치 (2,000,000,000원) - " + m.get(Rank.FIRST) + "개");
-
-        System.out.printf("총 수익률은 %.1f%%입니다.%n", yield);
+        System.out.printf(MESSAGE_YIELD_FORMAT, yield);
     }
 }
