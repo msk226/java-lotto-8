746ae139e11e48c9c50c86862eddb6d669be5804
msk226 <kminseok5167@gmail.com>
2025-11-01 14:35:50 +0900

    refactor(domain): LottoResultTest 내 메서드 사이 공백 제거 - 컨벤션 유지

--- src/test/java/lotto/domain/LottoResultTest.java
+++ src/test/java/lotto/domain/LottoResultTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class LottoResultTest {
-
     @Test
     void 로또_결과를_생성할_수_있다() {
         LottoResult lottoResult = new LottoResult(List.of(FIRST, SECOND));
